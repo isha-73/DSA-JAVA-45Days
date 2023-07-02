a5e7b1b0355fe6db04583688f70e0ec9a2b50dc8
Isha Bule <ishabule753@gmail.com>
2023-07-03 00:01:01 +0530

    Arrays1

---
+++ Arrays/SetMAtrixZero2.java
@@ -0,0 +1,70 @@
+//SPACE O(1)
+
+class Solution {
+    // Space O(1)
+    public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        // Check if the first row contains zero
+        for (int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column contains zero
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and column as markers to identify the rows and columns to be set to zero
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set zeros in the rows based on the markers in the first column
+        for (int i = 1; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < cols; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set zeros in the columns based on the markers in the first row
+        for (int j = 1; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < rows; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set zeros in the first row if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Set zeros in the first column if needed
+        if (firstColHasZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
---
+++ Arrays/SetMatrixZeros1.java
@@ -0,0 +1,45 @@
+// space O(m*n)
+class Solution { 
+    public void setZeroes(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] grid= new int [n][m];
+
+           for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                    grid[i][j]=matrix[i][j];
+            }
+            }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(matrix[i][j]==0){
+                    modify(grid,i,j);
+                    
+                }
+            }
+        }
+
+             for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                    matrix[i][j]=grid[i][j];
+            }
+            }
+    }
+
+    static void modify(int [][]arr, int row,int col){
+      
+        int n=arr.length;
+        int m=arr[0].length;
+        // 0 in row
+        for(int i=0;i<m;i++){
+            arr[row][i]=0;
+        }
+         // 0 in col
+        for(int i=0;i<n;i++){
+            arr[i][col]=0;
+        }
+    
+    }
+}
